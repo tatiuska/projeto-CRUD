7a4894bd23a8184803fec750361b63d13a3e0273
tatiuska <tfweska@gmail.com>
2023-06-14 18:44:34 -0300

    Criação do arquivo conexao.php para conexão com o banco de dados criado (empresa)

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php 
+    // Código para a conexão com o banco de dados.
+
+    $server = "localhost";
+    $user = "admin";
+    $password = "9*uPal2SrYIQ9M74";
+    $bd = "empresa";
+
+    // Estrutura condicional para aplicação da função mysqli_connect, que retorna True ou False para a conexão com o banco de dados.
+    if (mysqli_connect($server, $user, $password, $bd)) {
+        // echo "Conexão realizada com sucesso!"; - Essa linha foi usada para testar a conexão.
+    } else
+        echo "Erro de conexão!";
+?>
