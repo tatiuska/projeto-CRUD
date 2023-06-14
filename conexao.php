f71ce3a54badb7bae4dc215e04cd05dae036378a
tatiuska <tfweska@gmail.com>
2023-06-14 19:06:33 -0300

    Adição da variável conexão para receber a função mysqli_connect

--- conexao.php
+++ conexao.php
@@ -7,7 +7,7 @@
     $bd = "empresa";
 
     // Estrutura condicional para aplicação da função mysqli_connect, que retorna True ou False para a conexão com o banco de dados.
-    if (mysqli_connect($server, $user, $password, $bd)) {
+    if ($conexao = mysqli_connect($server, $user, $password, $bd)) {
         // echo "Conexão realizada com sucesso!"; - Essa linha foi usada para testar a conexão.
     } else
         echo "Erro de conexão!";
